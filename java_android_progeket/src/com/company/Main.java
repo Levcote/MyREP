6f1f5b63f5294d45525c984169aae3454f79b155
Levcote <l.berezovskiy@student.khai.edu>
2020-02-25 16:50:33 +0200

    abstract class

--- java_android_progeket/src/com/company/Main.java
+++ java_android_progeket/src/com/company/Main.java
@@ -1,37 +1,12 @@
 package com.company;
-import java.util.Scanner;
-import java.lang.String;
+
 public class Main {
 
     public static void main(String[] args) {
-    Scanner RTX_P99_of_on = new Scanner(System.in);
-    boolean num_RTX = RTX_P99_of_on.nextBoolean();
-    Computer RTX_P99 = new Computer();
-
-    Scanner of_on =new Scanner(System.in);
-    boolean num= of_on.nextBoolean();
-    Computer PS_1 =new Computer();
-
-    if (num_RTX == true){
-        RTX_P99.i9.Dat_analysis();
-        RTX_P99.transfer.Dat_analysis();
-
-        }
-    else {
-        RTX_P99.i9.shutdown();
-        RTX_P99.transfer.shutdown();
-    }
-    if(num==true){
-        PS_1.i5.start();
-        PS_1.transfer.start();
-    }
-    else{
-        PS_1.i5.shutdown();
-        PS_1.transfer.shutdown();
-    }
-
-
-
+        Shape_1 b = new Circle();
+        Shape_1 a = new Circle();
+        a.drove();
+        b.drove();
     }
 }
 
